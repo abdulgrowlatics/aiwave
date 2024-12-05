d53e1675316b47377410cf0fbfabaad47ea0bbdb
abdulgrowlatics <abdulrehmangrowlatics@gmail.com>
2024-12-05 21:48:35 +0500

    Update index.js

--- pages/terms-policy/index.js
+++ pages/terms-policy/index.js
@@ -12,18 +12,7 @@ const TermsPolicyPage = () => {
     <>
       <PageHead title="Terms and Policy" />
 
-      <main className="page-wrapper rbt-dashboard-page bg-dark">
-        <Context>
-          <div className="rbt-panel-wrapper">
-            <HeaderDashboard display="d-none" />
-            <PopupMobileMenu />
-            <LeftDashboardSidebar />
-
-            {/* <TermsPolicy /> */}
-            <CommingSoon />
-          </div>
-        </Context>
-      </main>
+      
     </>
   );
 };
