5b264fa1454ab369942d8e37ae254832be273ba3
abdulgrowlatics <abdulrehmangrowlatics@gmail.com>
2024-12-05 21:40:44 +0500

    Update index.js

--- pages/email-generator/index.js
+++ pages/email-generator/index.js
@@ -17,32 +17,7 @@ const EmailGeneratorPage = () => {
     <>
       <PageHead title="Email Generator" />
 
-      <main className="page-wrapper rbt-dashboard-page">
-        <div className="rbt-panel-wrapper">
-          <Context>
-            <LeftDashboardSidebar />
-            <HeaderDashboard display="" />
-            <RightDashboardSidebar />
-            <Modal />
-            <PopupMobileMenu />
-
-            <div className="rbt-main-content">
-              <div className="rbt-daynamic-page-content">
-                <div className="rbt-dashboard-content">
-                  <div className="content-page">
-                    <div className="chat-box-section">
-                      <EmailGenerator />
-                      <StaticbarDashboard />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <BackToTop />
-          </Context>
-        </div>
-      </main>
+     
     </>
   );
 };
