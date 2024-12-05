3693d1664760e3f3be86fc862e0679a9442e6b0a
abdulgrowlatics <abdulrehmangrowlatics@gmail.com>
2024-12-05 21:39:48 +0500

    Update index.js

--- pages/image-editor/index.js
+++ pages/image-editor/index.js
@@ -18,33 +18,7 @@ const ImageEditorPage = () => {
     <>
       <PageHead title="Image Editor" />
 
-      <main className="page-wrapper rbt-dashboard-page bg-dark">
-        <div className="rbt-panel-wrapper">
-          <Context>
-            <LeftDashboardSidebar />
-            <HeaderDashboard display="" />
-            <RightDashboardSidebar />
-            <Modal />
-            <PopupMobileMenu />
-
-            <div className="rbt-main-content">
-              <div className="rbt-daynamic-page-content">
-                <div className="rbt-dashboard-content">
-                  <div className="content-page">
-                    <div className="chat-box-section">
-                      <CommingSoon />
-                      {/* <ImageEditor /> */}
-                      {/* <StaticbarDashboard /> */}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <BackToTop />
-          </Context>
-        </div>
-      </main>
+      
     </>
   );
 };
